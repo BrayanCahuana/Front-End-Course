a2216030bd87fafbad52cab6185637eae61008af
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-03-15 20:02:50 -0500

    avances de semanas

---
+++ semana4/dia1/variables/java.js
@@ -0,0 +1,17 @@
+// este es un comentario de una linea
+
+/*
+este es un comentario de 
+varias lineas
+*/
+
+
+//variable de tipo texto
+let nombre = "Brayan"
+let sistemaOperativo = "linux"
+console.log; typeof(nombre) //esto me devuelve el string
+
+//alert ("mis segundo nombre es"+nombre +"y mi sistema operativo es"+sistemaOperativo);
+
+//alert ('mi segundo nombre es ${nombre}');
+alert(`Mi nombre es ${nombre} y uso el sistema operativo ${sistemaOperativo}`);
