4b44c5ff46d7ad68a68a881779298a78983fb901
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-03-16 19:25:01 -0500

    avanzes js

--- semana4/dia1/variables/java.js
+++ semana4/dia1/variables/java.js
@@ -5,13 +5,78 @@ este es un comentario de
 varias lineas
 */
 
-
 //variable de tipo texto
-let nombre = "Brayan"
-let sistemaOperativo = "linux"
-console.log; typeof(nombre) //esto me devuelve el string
+let nombre = "Brayan";
+let sistemaOperativo = "linux";
+console.log;
+typeof nombre; //esto me devuelve el string
 
-//alert ("mis segundo nombre es"+nombre +"y mi sistema operativo es"+sistemaOperativo);
+//alert(`Mi nombre es ${nombre} y uso el sistema operativo ${sistemaOperativo}`);
 
-//alert ('mi segundo nombre es ${nombre}');
-alert(`Mi nombre es ${nombre} y uso el sistema operativo ${sistemaOperativo}`);
+//variable de tipo numero
+let miedad = "16";
+let peso = "52.1";
+let altura = "1.70";
+let bmi = peso / altura ** 2;
+alert(bmi);
+
+//la suma +
+//la mutiplicacion *
+//la division /
+//el modulo %
+let a = 6;
+let b = 10;
+let suma = a + b;
+console.log(suma);
+let resta = b - a;
+console.log(resta);
+let multiplicacion = b * a;
+console.log(multiplicacion);
+let division = b / a;
+console.log(division);
+let modulo = b % a;
+console.log(modulo);
+let potenciacion = b ** a;
+console.log(potenciacion);
+
+// declarar una variable para la formula de grados centigrados a farenheit
+let celsius = 50
+let farenheit = ((celsius)*(9/5)+32)
+alert (farenheit)
+
+//farenheit a celsius
+let farenheit1 = 2;
+let celsius1 = ((farenheit1-32)*(5/9));
+alert(celsius1);
+
+
+
+let brayan = {
+    primernombre : "Alexandro",
+    apellidopaterno : "Cahuana",
+    apellidomaterno : "Utani",
+};
+let nombrecompleto = `${brayan.primernombre} ${brayan.apellidopaterno} ${brayan.apellidomaterno}`;
+alert (nombrecompleto)
+
+
+
+
+//vamos a crear un objeto donde almacenaremos nuestros hobbies favoritos , numero de mascotas que tenemos y ademas si somos hombres le damos true/false e igual con mujeres
+// vamos a armar un alerta que reciba como datos nuestros nombres y de un mensaje de bienvenida
+// vamos a escribir la formula cuadratica
+
+
+let datos = {
+    hobbies : "dormir" ,
+    mascotas : "tres" ,
+    hombre : true ,
+    mujer : false ,
+
+};
+let alerta = `mis hobbies son ${datos.hobbies},cantidad de mascotas que tengo ${datos.mascotas}, soy ${datos.genero}`
+alert (alerta)
+
+let nuestronombre = "nombre de ususario"
+alert (nuestronombre ,'Bienvenido a mi pagina en blanco :v')
+document.write ("alerta")
---
+++ semana4/dia2/condicionales/javascript.js
@@ -0,0 +1,50 @@
+//condicionales
+
+//if (se cumple esta condicion) {
+    //mi codigo que se ejecute si es veerdadero
+//}else if (si se cumple esta otra condicon) {
+//mi codigo si se ejecuta esta otra condicion
+//}else if (si se cumple esta otra condicon) {
+//mi codigo si se ejecuta esta otra condicion
+//}
+
+
+let condicion = false ;
+if (condicion === true) {
+    console.log ("es verdadero")
+}
+else {
+    console.log ("es falso")
+}
+
+
+
+
+
+let a=0
+let b=0
+
+a=5
+b=4
+
+if (a == 5) {
+    console.log ("adivinaste")
+    //alert (`${a}`)
+}
+else {
+    console.log ("fallaste")
+}
+
+let lenguajedeprogramacion = ""
+if (lenguajedeprogramacion == "pyton") {
+    console.log ("python es xvr")
+}
+else if(lenguajedeprogramacion == "js") {
+    console.log ("js es mejor")
+}
+else if(lenguajedeprogramacion == "php") {
+    console.log ("js es mejor")
+}
+else if(lenguajedeprogramacion == "") {
+    console.log ("no le sabes? o_O")
+}
