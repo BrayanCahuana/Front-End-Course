0417645a9796476e0a311597f1c044acab429ad3
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-04-26 20:02:23 -0500

    .

--- pruebas/js.js
+++ pruebas/js.js
@@ -1,3 +1,9 @@
-let x = 20
-x *= 5
-console.log(x)
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((number) => number * 2);
+console.log(doubled);
+
+
+
+let miArray = [1, 2, 4, 8];
+let transformado = miArray.map(item => item * 2);
+console.log(transformado);
